116121048079ed66488a8424585484b5e5c02bd0
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-09-27 10:34:00 +0200

    Testowanie formularza logowania.

--- ComarchTestExplorer/Tests/E2e/ComarchTestExplorer.E2e.Tests/HerokuappTests.cs
+++ ComarchTestExplorer/Tests/E2e/ComarchTestExplorer.E2e.Tests/HerokuappTests.cs
@@ -43,4 +43,23 @@ public class HerokuappTests
 
         Assert.That(successMessage.Text, Does.Contain("You logged into a secure area!"));
     }
+
+    [Test]
+    public void IncorrectLoginTest()
+    {
+        driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+        var usernameField = driver.FindElement(By.Id("username"));
+        var passwordField = driver.FindElement(By.Id("password"));
+
+        usernameField.SendKeys("wrongUser");
+        passwordField.SendKeys("wrongPassword");
+
+        var loginButton = driver.FindElement(By.CssSelector("button[type='submit']"));
+        loginButton.Click();
+
+        var errorMessage = driver.FindElement(By.Id("flash"));
+        Assert.That(errorMessage.Text, Does.Contain("Your username is invalid!"), "Niepoprawne dane logowania nie wywołały błędu.");
+    }
+
 }
